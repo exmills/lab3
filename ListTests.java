36c913b0667a6fd3106a1f4fa7630116cdf657f0
exmills <114551044+exmills@users.noreply.github.com>
2022-10-16 22:41:46 -0700

    Create ListTests.java

---
+++ ListTests.java
@@ -0,0 +1,24 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class StringChecker {
+    public boolean checkString(String stringj) {
+        if (stringj.length() == 5) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
+
+public class ListTests {
+    @Test
+    public void testingFilter() {
+        List<String> newList = new ArrayList<"hello", "goodbye", "first", "why", "yes", "nosir">;
+        assertArrayEquals(<"hello", "first">, 
+        
+    }
+
+
+}
